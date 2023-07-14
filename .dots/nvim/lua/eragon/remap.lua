a8175b2361b4e15be77cbb5ef38168413c9d59fc
ERAGON007 <turk6486@gmail.com>
2023-07-14 10:48:18 +0330

    Restructure to use with bombadil dotfile manager

---
+++ .dots/nvim/lua/eragon/remap.lua
@@ -0,0 +1,12 @@
+
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- Some Clipboard stuff
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+
